34b514efba4d0cecd4ba2b117a1f9a1feaf99572
divanshuR <divanshu.rohatgi6@gmail.com>
2018-02-26 19:51:03 +0530

    adview added

--- app/src/main/java/com/rohatgi/developer/factcheck/TriviaActivity.java
+++ app/src/main/java/com/rohatgi/developer/factcheck/TriviaActivity.java
@@ -17,10 +17,16 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 import java.util.Random;
 
 public class TriviaActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +37,42 @@ public class TriviaActivity extends AppCompatActivity {
         final TextView show=  findViewById(R.id.show);
         final EditText triviatext=  findViewById(R.id.triviaText);
         final Button btntrivia=  findViewById(R.id.btntrivia);
+
+
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        mAdView.setAdListener(new AdListener(){
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when when the user is about to return
+                // to the app after tapping on an ad.
+            }
+        });
 
 
         triviatext.setOnFocusChangeListener(new View.OnFocusChangeListener() {
