e6179732cad43478ce189ef6b17a99a449c59ad3
divanshuR <divanshu.rohatgi6@gmail.com>
2018-03-09 01:30:21 +0530

    Final update with UI changes

--- app/src/main/java/com/rohatgi/developer/factcheck/AboutActivity.java
+++ app/src/main/java/com/rohatgi/developer/factcheck/AboutActivity.java
@@ -20,13 +20,13 @@ public class AboutActivity extends AppCompatActivity {
         View aboutPage = new AboutPage(this)
                 .isRTL(false)
                 .setImage(R.drawable.about)
-                .setDescription("Interesting|Hidden and Meaningful Facts About Numbers")
+                .setDescription("Interesting|Hidden and Meaningful Facts About Numbers. Make sure you're always connected to internet and learning.")
                 .addItem(new Element().setTitle("Version 2.0"))
                 .addGroup("Connect with the Developer")
                 .addFacebook("divanshu.rohatgi6")
                 .addInstagram("divanshurohatgi")
                 .addGitHub("DivanshuR")
-                
+
                 .create();
 
         setContentView(aboutPage);
